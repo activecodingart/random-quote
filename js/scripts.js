2f4cc242374872720ea0f1d631bcb8015f2445b6
ActiveCodingArt <piotrbrzezicki81@gmail.com>
2016-08-14 19:12:50 +0100

    Added project files

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(function() {
+	
+	var tweetLink = "https://twitter.com/intent/tweet?text=",
+		quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
+	
+	function getQuote() {
+		$.getJSON( quoteUrl, createTweet );
+	}
+	
+	function createTweet(input) {
+		if (!input.quoteAuthor.length) {
+			input.quoteAuthor = "Unknown author";
+		}
+		
+		var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+		
+		if (tweetText.length > 140) {
+			getQuote();
+		} else {
+			var tweet = tweetLink + tweetText;
+			$('.quote').text(input.quoteText);
+			$('.author').text("Author: " + input.quoteAuthor);
+			$('.tweet').attr('href', tweet);
+		}
+	}
+	
+	getQuote();
+	
+	var button = $('.trigger').click(function() {
+		getQuote();
+	});
+	
+});
